6975c066ec389033cd56b327d934c205eb535cb0
SAM-LAPTOP\sambu <sambutler1017@icloud.com>
2022-04-29 23:36:24 -0400

    Cleaning up code

--- dist/index.js
+++ dist/index.js
@@ -8771,12 +8771,21 @@ let CURRENT_VERSION = { major: 0, minor: 0, fix: 0 };
 let NEW_VERSION = { major: 0, minor: 0, fix: 0 };
 let RELEASE_DATA = "New Release";
 
+/**
+ * This will run the async process to create the version bump.
+ * It will first get the latest tag from the repository and determine
+ * if it needs a major, minor, or patch version update based on
+ * the commit message that was sent with the merge.
+ *
+ * @author Sam Butler
+ * @since April 29, 2022
+ */
 async function run() {
   setup();
   getLatestTag().then(() => {
     getLatestCommitMessage().then(() => {
       try {
-        const version = buildVersionString(NEW_VERSION);
+        const version = parseVersion(NEW_VERSION);
         console.log("Setting outputs...");
         core.setOutput("tag", `v${version}`);
         core.setOutput("release_name", `Release v${version}`);
@@ -8788,15 +8797,24 @@ async function run() {
   });
 }
 
+/**
+ * This will setup the owner and repository name to be used
+ * when getting the tags and commit messages.
+ */
 function setup() {
   try {
     owner = github.context.repo.owner;
     repo = github.context.repo.repo;
   } catch (error) {
-    // core.setFailed("Could not get owner or repo name");
+    core.setFailed("Could not get owner or repo name");
   }
 }
 
+/**
+ * Async function to get all the tags from the repository and
+ * then pull the latest one. It will then set the class level
+ * variables of the {@link NEW_VERSION} and {@link CURRENT_VERSION}.
+ */
 async function getLatestTag() {
   console.log("Getting latest tags...");
   const tags = await octokit.request("GET /repos/{owner}/{repo}/tags", {
@@ -8814,9 +8832,16 @@ async function getLatestTag() {
   };
   NEW_VERSION = { ...CURRENT_VERSION };
 
-  console.log(`Latest tag found: 'v${buildVersionString(CURRENT_VERSION)}'\n`);
+  console.log(`Latest tag found: 'v${parseVersion(CURRENT_VERSION)}'\n`);
 }
 
+/**
+ * This will get the latest commit message to be used on the release.
+ * It will check see what bump version it should do and then it will
+ * pull the commit message and attach it to the release notes. If the
+ * message can not be determined then it will do a default patch and
+ * add a link to the diff from the previous version.
+ */
 async function getLatestCommitMessage() {
   console.log("Getting last commit message...");
   const commits = await octokit.request("GET /repos/{owner}/{repo}/commits", {
@@ -8824,8 +8849,25 @@ async function getLatestCommitMessage() {
     repo: repo,
   });
 
+  bumpVersion(commits.data[0].commit.message);
+
+  buildReleaseBody(
+    parseVersion(CURRENT_VERSION),
+    parseVersion(NEW_VERSION),
+    getCommitBody(commits.data[0].commit.message)
+  );
+}
+
+/**
+ * This will bump the current version that was pulled. If there is
+ * not a bump type on the commit message then it will do a patch
+ * by default.
+ *
+ * @param message The commit message to parse.
+ */
+function bumpVersion(message) {
   const regexEx = new RegExp("<(.*?)>(:.*)");
-  let result = regexEx.exec(commits.data[0].commit.message);
+  let result = regexEx.exec(message);
 
   const bumpType = result ? result[1] : "patch";
   if (bumpType === "major") {
@@ -8839,29 +8881,42 @@ async function getLatestCommitMessage() {
     NEW_VERSION.fix = NEW_VERSION.fix + 1;
   }
 
-  console.log(`Commit message found: '${commits.data[0].commit.message}'`);
-  console.log(
-    `New Version Bump: ${bumpType} -> 'v${buildVersionString(NEW_VERSION)}'\n`
-  );
+  console.log(`Commit message found: '${message}'`);
+  console.log(`New Version: ${bumpType} -> 'v${parseVersion(NEW_VERSION)}'\n`);
+}
 
+/**
+ * This will get the body of the commit. If there is no commit
+ * body then it will just set a default release body.
+ *
+ * @param message The commit message to parse.
+ * @returns The body message to add to to the release notes.
+ */
+function getCommitBody(message) {
+  const regexEx = new RegExp("<(.*?)>(:.*)");
+  let result = regexEx.exec(message);
   let bodyMesage = "New Release";
+
   if (result) {
     bodyMesage = result[2].replace(": ", "");
   } else {
     const regexEx = new RegExp("(:.*)");
-    result = regexEx.exec(commits.data[0].commit.message);
-    bodyMesage = result
-      ? result[0].replace(":", "").trim()
-      : commits.data[0].commit.message;
+    result = regexEx.exec(message);
+    bodyMesage = result ? result[0].replace(":", "").trim() : message;
   }
-
-  buildReleaseBody(
-    buildVersionString(CURRENT_VERSION),
-    buildVersionString(NEW_VERSION),
-    bodyMesage
-  );
+  return bodyMesage;
 }
 
+/**
+ * Builds out the release body to be used so it is formatted correctly
+ * for github. It will have a link to compare the new version to the
+ * previous version and also the most recent commit message that was
+ * merged with the new release.
+ *
+ * @param currentVersion The current version on github.
+ * @param newVersion The new version that should be created.
+ * @param body The message to add to the releease.
+ */
 function buildReleaseBody(currentVersion, newVersion, body) {
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, "0");
@@ -8873,7 +8928,14 @@ function buildReleaseBody(currentVersion, newVersion, body) {
   console.log("Content: \n" + RELEASE_DATA + "\n");
 }
 
-function buildVersionString(version) {
+/**
+ * This will take the version and parse it into a readable
+ * and formatted version string.
+ *
+ * @param version The version to format.
+ * @returns The new formatted string.
+ */
+function parseVersion(version) {
   return `${version.major}.${version.minor}.${version.fix}`;
 }
 
